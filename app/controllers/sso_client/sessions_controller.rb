362e57ec0d78ed45fe14abd54ab2221375ed4183
Dmitry Lihachev <lda@openteam.ru>
2011-12-13 13:26:25 +0700

    #8939. single sign out

---
+++ app/controllers/sso_client/sessions_controller.rb
@@ -0,0 +1,19 @@
+class SsoClient::SessionsController < Devise::SessionsController
+  def new
+    redirect_to '/sso/auth/identity'
+  end
+  def destroy
+    sign_out :user
+    redirect_to "#{Settings['sso_provider.host']}/users/sign_out?redirect_uri=#{CGI.escape(redirect_uri)}"
+  end
+
+  protected
+
+    def redirect_uri
+      URI.parse(request.url).tap do | uri |
+        uri.path = main_app.root_path
+        uri.query = nil
+      end.to_s
+    end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,10 @@ Rails.application.routes.draw do
              :path => 'sso',
              :skip => [:registrations, :passwords, :confirmations],
              :controllers => {:omniauth_callbacks => "sso_client/omniauth_callbacks"} do
-    get 'sign_in' => redirect('/sso/auth/identity'), :as => :new_user_session
-    get "sign_out", :to => "devise/sessions#destroy"
+    get '/sso/sign_in' => redirect('/sso/auth/identity'), :as => :new_user_session
+    get "/sso/sign_out", :to => "sso_client/sessions#destroy"
   end
 
-  get '/sso/auth/identity', :as => :single_sign_on
+  get '/sso/auth/identity', :as => :sign_on
+  get "/sso/sign_out", :as => :sign_out
 end
