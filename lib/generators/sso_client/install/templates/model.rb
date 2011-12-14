ccd88aca04499772c20acf95547b56158357d9f9
Dmitry Lihachev <lda@openteam.ru>
2011-12-14 11:11:39 +0700

    annotate user model

--- lib/generators/sso_client/install/templates/model.rb
+++ lib/generators/sso_client/install/templates/model.rb
@@ -14,19 +14,25 @@ end
 #
 # Table name: users
 #
-#  id                     :integer         not null, primary key
-#  email                  :string(255)     default(""), not null
-#  encrypted_password     :string(128)     default(""), not null
-#  reset_password_token   :string(255)
-#  reset_password_sent_at :datetime
-#  remember_created_at    :datetime
-#  sign_in_count          :integer         default(0)
-#  current_sign_in_at     :datetime
-#  last_sign_in_at        :datetime
-#  current_sign_in_ip     :string(255)
-#  last_sign_in_ip        :string(255)
-#  created_at             :datetime
-#  updated_at             :datetime
-#  name                   :string(255)
+#  id                 :integer         not null, primary key
+#  uid                :string(255)
+#  name               :text
+#  email              :text
+#  nickname           :text
+#  first_name         :text
+#  last_name          :text
+#  location           :text
+#  description        :text
+#  image              :text
+#  phone              :text
+#  urls               :text
+#  raw_info           :text
+#  sign_in_count      :integer         default(0)
+#  current_sign_in_at :datetime
+#  last_sign_in_at    :datetime
+#  current_sign_in_ip :string(255)
+#  last_sign_in_ip    :string(255)
+#  created_at         :datetime
+#  updated_at         :datetime
 #
 
