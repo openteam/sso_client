5a42c8f1362d9c39f5f3a125c3cf6acb83fbdc8c
Dmitry Lihachev <lda@openteam.ru>
2012-01-30 12:37:05 +0700

    rf deprecated code

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,11 @@ Rails.application.routes.draw do
   devise_for  :users,
               :path => 'sso',
               :skip => [:registrations, :passwords, :confirmations],
-              :controllers => {:omniauth_callbacks => "sso_client/omniauth_callbacks"} do
+              :controllers => {:omniauth_callbacks => "sso_client/omniauth_callbacks"}
 
+  devise_scope :users do
     get '/sso/sign_in' => redirect('/sso/auth/identity'), :as => :new_user_session
     get "/sso/sign_out", :to => "sso_client/sessions#destroy", :as => :destroy_user_session
-
   end
 
   get '/sso/auth/identity', :as => :sign_on
